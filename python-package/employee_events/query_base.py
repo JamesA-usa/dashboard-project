3c57801129eedc126765a8b5e55a3e3e30efe618
David Brinkjans <135713199+DavidB1999@users.noreply.github.com>
2025-08-10 18:38:22 +0200

    Started working on query base

--- python-package/employee_events/query_base.py
+++ python-package/employee_events/query_base.py
@@ -1,27 +1,28 @@
 # Import any dependencies needed to execute sql queries
-# YOUR CODE HERE
+from sqlite3 import connect
+from employee_events import QueryMixin
 
 # Define a class called QueryBase
 # Use inheritance to add methods
 # for querying the employee_events database.
-# YOUR CODE HERE
-
+class QueryBase(QueryMixin):
+    
     # Create a class attribute called `name`
     # set the attribute to an empty string
-    # YOUR CODE HERE
+    name=''
 
     # Define a `names` method that receives
     # no passed arguments
-    # YOUR CODE HERE
+    def names(self):
         
         # Return an empty list
-        # YOUR CODE HERE
+        return []
 
 
     # Define an `event_counts` method
     # that receives an `id` argument
     # This method should return a pandas dataframe
-    # YOUR CODE HERE
+    def event_counts(self, id):
 
         # QUERY 1
         # Write an SQL query that groups by `event_date`
