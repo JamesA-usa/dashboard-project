b4ccea3eccb7e9b62ad8f86beb777c0bd1b0a823
David Brinkjans <135713199+DavidB1999@users.noreply.github.com>
2025-08-24 20:11:20 +0200

    Fixed db_path in sql_execution

--- python-package/employee_events/sql_execution.py
+++ python-package/employee_events/sql_execution.py
@@ -5,8 +5,7 @@ import pandas as pd
 
 # Using pathlib, create a `db_path` variable
 # that points to the absolute path for the `employee_events.db` file
-db_path = Path(__file__).parent.parent / 'python-package/employee_events/employee_events.db'
-
+db_path = Path(__file__).resolve().parent / 'employee_events.db'
 
 # OPTION 1: MIXIN
 # Define a class called `QueryMixin`
